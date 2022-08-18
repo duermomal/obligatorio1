b8111523e29d48ed2bb6993adcd8429c450a2ba7
duermomal <110434883+duermomal@users.noreply.github.com>
2022-08-18 00:55:16 -0300

    login con validaciones

--- js/index.js
+++ js/index.js
@@ -1,5 +1,22 @@
-const ingresar = document.getElementById("ingreso");
+const form = document.getElementById("formulario");
 
-ingresar.addEventListener('click', () =>{
-    window.location = "principal.html"
+form.addEventListener('submit', e =>{
+    e.preventDefault();
+    const email = document.getElementById("email");
+    const contra = document.getElementById("contra");
+    const alerta = document.getElementById("warning");
+    if(email.value.length == 0 && contra.value.length == 0){
+        alerta.innerHTML = `los campos no pueden ser vacíos.`;
+        
+    }
+    else if(email.value.length == 0){
+        alerta.innerHTML = `el email no puede ser vacío.`;
+    }
+    else if(contra.value.length == 0) {
+        alerta.innerHTML = `la contraseña no puede ser vacía.`;
+    }
+    else {
+        window.location = "principal.html";
+    }
+    
 });
